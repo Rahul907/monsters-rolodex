1d3dc058bc9d6c9b63c3c2e0cf127f35fee4cb01
Rahul907 <rahulmaurya907@gmail.com>
2020-08-14 06:14:30 +0530

    Updates

---
+++ precache-manifest.1323b937880d61d0e81f1f61baf39a4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "550d6509ee7a257716ace45e8340c3c3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9a86b204970ef752acd3",
+    "url": "/monsters-rolodex/static/css/main.90b42c3b.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a86b204970ef752acd3",
+    "url": "/monsters-rolodex/static/js/main.2344143b.chunk.js"
+  },
+  {
+    "revision": "46fcc5578023f1586c35",
+    "url": "/monsters-rolodex/static/js/runtime-main.7affd505.js"
+  }
+]);
